478e82104f47d7fe95adc8d0c823bb01fb2a2118
THEcryptoCAP <cryptocapinoid@gmail.com>
2026-08-08 17:21:06 +0530

    more functions

--- src/balance.rs
+++ src/balance.rs
@@ -16,5 +16,9 @@ impl Pallet {
     pub fn set_balance(&mut self, who: &String, amount: u128){
         self.balances.insert(key: who.clone(), value: amount);
     }
-    
+    //fetching balance by name
+    pub fn balance(&self, who: &String) -> u128{
+        self.balances.get(key:who).unwrap_or(default: &0)
+    }
+
 }
